0cdd4a676134ca7dcc431ba7235643ced992c3e4
jaeml00 <jaeml0630@gmail.com>
2023-05-17 21:01:06 +0900

    자료 구조, 우선순위 큐

---
+++ py/11286.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+
+data = [int(input()) for _ in range(N)]
+list = []
+
+for i in range(N):
+    if data[i] == 0:
+        if len(list):
+            print(heapq.heappop(list)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(list, (abs(data[i]), data[i]))
