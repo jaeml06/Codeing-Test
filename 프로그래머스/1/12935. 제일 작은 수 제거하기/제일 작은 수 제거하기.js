574e5735a9db498f5e9099266103c3294d9cea5f
jaeml06 <107801932+jaeml06@users.noreply.github.com>
2024-04-14 10:12:23 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+    
+    
+    var answer = [];
+ 
+    
+    if (arr.length<=1){
+        return [-1];
+
+    }
+        arr.splice(arr.indexOf(Math.min(...arr)),1);
+        answer =  arr;
+ 
+    
+    return answer
+}
