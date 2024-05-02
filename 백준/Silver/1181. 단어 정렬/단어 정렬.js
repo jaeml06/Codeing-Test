158b36539d13b1e699f74b30a7e13f35624a7a96
jaeml06 <107801932+jaeml06@users.noreply.github.com>
2024-05-02 21:53:09 +0900

    [Silver V] Title: 단어 정렬, Time: 236 ms, Memory: 19544 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,9 @@
+let input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+
+let setarr = [...new Set(input)];
+
+setarr.shift();
+
+let result = setarr.sort().sort((a, b) => a.length - b.length).join("\n");
+
+console.log(result)
