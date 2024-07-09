4ba10f431149bbded9071c0c98abd62905a2e019
jaeml06 <107801932+jaeml06@users.noreply.github.com>
2024-07-09 16:06:38 +0900

    [Silver IV] Title: 대칭 차집합, Time: 440 ms, Memory: 88720 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.js
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.js
@@ -1,12 +1,12 @@
 let fs = require('fs');
 let read1 = fs.readFileSync(0).toString().trim().split('\n');
 const A = read1[1].split(' ').map(Number);
-const B = read1[2].split(' ').map(Number);
-const setB = new Set(B)
+const B = new Set(read1[2].split(' ').map(Number));
+
 let answer = new Set([...A, ...B]);
 
 A.forEach((a) => {
-  if(setB.has(a)){
+  if(B.has(a)){
     answer.delete(a)
   }
 })
