8310d7449372d29110e395a4ce0984fb1406298a
jaeml06 <107801932+jaeml06@users.noreply.github.com>
2024-08-20 01:07:34 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,16 @@
+import sys
+read = sys.stdin.readline
+
+num1 = int(read())
+
+num2 = read()
+
+ 
+
+print(num1*int(num2[2]))
+
+print(num1*int(num2[1]))
+
+print(num1*int(num2[0]))
+
+print(num1*int(num2))
