093ba89826d641d8feb36bb8dba869a7750691c1
jaeml06 <107801932+jaeml06@users.noreply.github.com>
2024-09-24 23:16:45 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 53.92 ms, Memory: 160 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+    const answer = new Array(numbers.length).fill(-1);
+    const stack = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        while (stack.length > 0 && numbers[stack[stack.length - 1]] < numbers[i]) {
+            const index = stack.pop();
+            answer[index] = numbers[i];
+        }
+
+        stack.push(i);
+    }
+
+    return answer;
+}
