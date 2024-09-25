e926ca5c78a0fb904831d844c21e49a8ac856c44
jaeml06 <107801932+jaeml06@users.noreply.github.com>
2024-09-25 23:08:39 +0900

    [level 2] Title: 올바른 괄호, Time: 5.22 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 6.97 ms
+메모리: 37.7 MB, 시간: 5.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 24일 13:08:14
+2024년 09월 25일 23:08:36
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,19 @@
+function solution(s) {
+    var answer = true;
+
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (stack[stack.length - 1] === '(' && s[i] === ')') {
+            stack.pop();
+        } else if (s[i] === '(') {
+            stack.push('(');
+        } else {
+            return false;  
+        }
+    }
+
+    if (stack.length === 0) {
+        return true;
+    }
+    return false;
+}
