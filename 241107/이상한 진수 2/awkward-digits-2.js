c1e8c8f20a1e2bb253f250aa23fbd34bf1673442
jaeml06 <jaeml0630@gmail.com>
2024-11-07 12:38:47 +0900

    Title: 이상한 진수 2, Time: 185ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241107/이상한 진수 2/awkward-digits-2.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let read = fs.readFileSync(0).toString().trim().split('\n');
+
+const arr = [...read[0]].map(Number);
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] === 0){
+        arr[i] = 1;
+        break;
+    }
+}
+const answer = arr.reverse().reduce((prev, cur, index) => {
+    return prev + cur * Math.pow(2, index)
+}, 0)
+console.log(answer)
