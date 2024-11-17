8d65a7416d5722a3701199aec30f9bc8d1d273cd
jaeml06 <jaeml0630@gmail.com>
2024-11-17 23:51:49 +0900

    Title: 뿌요뿌요, Time: 185ms, Memory: 15MB, Status: Passed - CodeTree

--- 241117/뿌요뿌요/puyo-puyo.js
+++ 241117/뿌요뿌요/puyo-puyo.js
@@ -5,39 +5,34 @@ const n = Number(temp1);
 const graph = temp2.map(val => val.split(' ').map(Number))
 
 const visited = Array.from({length: n}, () => Array(n).fill(false));
-const dx = [-1, 1, 0, 0]
+const dx = [-1, 1, 0 , 0]
 const dy = [0, 0, -1, 1]
-const answer = []
 
-function dfs(y, x, flag){
+function dfs(x, y, flag){
     let count = 1
     for(let i = 0; i < 4; i++){
-        const ny = y + dy[i];
-        const nx = x + dx[i];
-        
-        if(ny >= 0 && ny < n && nx >= 0 && nx < n && 
-           !visited[ny][nx] && graph[ny][nx] === flag){
-            visited[ny][nx] = true;
-            count += dfs(ny, nx, flag);
+        const curX = x + dx[i];
+        const curY = y + dy[i];
+        if(0 <= curX && curX < n && 0 <= curY && curY < n && !visited[curY][curX] && flag === graph[curY][curX]){
+            visited[curY][curX] = true;
+            count += dfs(curX, curY, flag);
         }
     }
-    return count;
+    return count
 }
-
-for(let y = 0; y < n; y++){
-    for(let x = 0; x < n; x++){
-        if(!visited[y][x]){
-            visited[y][x] = true;
-            const count = dfs(y, x, graph[y][x]);
+let maxCount = Number.MIN_SAFE_INTEGER;
+let answer = 0
+for(let i = 0; i < n; i++){
+    for(let j = 0; j < n; j++){
+        if(!visited[i][j]){
+            visited[i][j] = true
+            const count = dfs(j, i, graph[i][j])
+            maxCount = Math.max(count, maxCount)
             if(count >= 4){
-                answer.push(count);
+                answer++
             }
         }
     }
 }
+console.log(answer + ' ' + maxCount)
 
-if(answer.length > 0){
-    console.log(answer.length + ' ' + Math.max(...answer));
-}else{
-    console.log('0 0');
-}
