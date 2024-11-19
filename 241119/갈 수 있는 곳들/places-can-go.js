48c213a94ff77a9424e6e5824a279bd637457b48
jaeml06 <jaeml0630@gmail.com>
2024-11-19 18:14:41 +0900

    Title: 갈 수 있는 곳들, Time: 180ms, Memory: 14MB, Status: Runtime Error - CodeTree

---
+++ 241119/갈 수 있는 곳들/places-can-go.js
@@ -0,0 +1,39 @@
+let fs = require('fs');
+let read = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = read.splice(0, 1)[0].split(' ').map(Number);
+const graph = read.splice(0, n).map(val => val.split(' ').map(Number));
+const dot = read.splice(0, k).map(val => val.split(' ').map(Number));
+
+const visited = Array.from({length : n}, () => Array(n).fill(false))
+
+const dx = [-1, 1, 0, 0]
+const dy = [0, 0, -1, 1]
+
+function bfs(x, y){
+    const queue = [[x, y]];
+    let head = 0
+    visited[0][0] = true;
+
+    while(head < queue.length){
+        const [x, y] = queue[head]
+        head++;
+        for(let i = 0; i < 4; i++){
+            const curX = x + dx[i];
+            const curY = y + dy[i];
+
+            if(0<= curX && curX < n && 0 <= curY && curY < n && !visited[curY][curX] && graph[curY][curX] === 0){
+                visited[curY][curX] = true;
+                queue.push([curX, curY]);
+            }
+        }
+    }
+    return head;
+}
+let answer = 0
+for(const [x, y] of dot){
+    if(!visited[y][x]){
+        answer += bfs(x, y)
+    }
+}
+
+console.log(answer)
