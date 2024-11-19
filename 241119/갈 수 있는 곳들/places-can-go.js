b94e5a079b71b9b35331b0b54696b233a6cc85aa
jaeml06 <jaeml0630@gmail.com>
2024-11-19 19:34:37 +0900

    Title: 갈 수 있는 곳들, Time: 203ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241119/갈 수 있는 곳들/places-can-go.js
+++ 241119/갈 수 있는 곳들/places-can-go.js
@@ -4,43 +4,44 @@ const [n, k] = read.splice(0, 1)[0].split(' ').map(Number);
 const graph = read.splice(0, n).map(val => val.split(' ').map(Number));
 const dot = read.splice(0, k).map(val => val.split(' ').map(Number));
 
-const visited = Array.from({length : n}, () => Array(n).fill(false))
+const visited = Array.from({ length: n }, () => Array(n).fill(false));
 
-const dx = [-1, 1, 0, 0]
-const dy = [0, 0, -1, 1]
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
 
-function bfs(x, y){
+function bfs(x, y) {
     const queue = [[x, y]];
-    let head = 0
-    visited[y][x] = true;
+    let head = 0;
+    visited[y][x] = true; 
 
-    while(head < queue.length){
-        const [x, y] = queue[head]
-        head++;
-        for(let i = 0; i < 4; i++){
-            const curX = x + dx[i];
-            const curY = y + dy[i];
+    while (head < queue.length) {
+        const [curX, curY] = queue[head++];
+        for (let i = 0; i < 4; i++) {
+            const nextX = curX + dx[i];
+            const nextY = curY + dy[i];
 
-            if(0<= curX && curX < n && 0 <= curY && curY < n && !visited[curY][curX] && graph[curY][curX] === 0){
-                visited[curY][curX] = true;
-                queue.push([curX, curY]);
+            if (
+                0 <= nextX && nextX < n &&
+                0 <= nextY && nextY < n &&
+                !visited[nextY][nextX] &&
+                graph[nextY][nextX] === 0
+            ) {
+                visited[nextY][nextX] = true;
+                queue.push([nextX, nextY]);
             }
         }
     }
-    return head;
+    return head; 
 }
-let answer = 0
-for(const [x, y] of dot){
+
+let answer = 0;
+for (const [x, y] of dot) {
     const startX = x - 1;
     const startY = y - 1;
 
-    if (
-        0 <= startX && startX < n &&
-        0 <= startY && startY < n &&
-        !visited[startY][startX]
-    ) {
+    if (!visited[startY][startX]) {
         answer += bfs(startX, startY);
     }
 }
 
-console.log(answer)
+console.log(answer);
