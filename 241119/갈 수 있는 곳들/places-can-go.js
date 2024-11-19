d904e2205be85e0a842d650fdbeb3daaf9b9bd31
jaeml06 <jaeml0630@gmail.com>
2024-11-19 20:03:24 +0900

    Title: 갈 수 있는 곳들, Time: 192ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241119/갈 수 있는 곳들/places-can-go.js
+++ 241119/갈 수 있는 곳들/places-can-go.js
@@ -1,31 +1,30 @@
 let fs = require('fs');
 let read = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k] = read.splice(0, 1)[0].split(' ').map(Number);
-const graph = read.splice(0, n).map(line => line.split(' ').map(Number));
-const dots = read.splice(0, k).map(line => line.split(' ').map(Number));
+const graph = read.splice(0, n).map(val => val.split(' ').map(Number));
+const dot = read.splice(0, k).map(val => val.split(' ').map(Number));
 
 const visited = Array.from({ length: n }, () => Array(n).fill(false));
-
 
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
 
 function bfs(x, y) {
     const queue = [[x, y]];
+    let count = 0
     let head = 0;
-    let count = 0;
-    visited[y][x] = true;
+    visited[y][x] = true; 
 
     while (head < queue.length) {
         const [curX, curY] = queue[head++];
-        count++;
+        count++
         for (let i = 0; i < 4; i++) {
             const nextX = curX + dx[i];
             const nextY = curY + dy[i];
 
             if (
-                nextX >= 0 && nextX < n &&
-                nextY >= 0 && nextY < n &&
+                0 <= nextX && nextX < n &&
+                0 <= nextY && nextY < n &&
                 !visited[nextY][nextX] &&
                 graph[nextY][nextX] === 0
             ) {
@@ -34,17 +33,17 @@ function bfs(x, y) {
             }
         }
     }
-    return count;
+    return count; 
 }
 
+let answer = 0;
+for (const [x, y] of dot) {
+    const startX = x - 1;
+    const startY = y - 1;
 
-let totalReachable = 0;
-dots.forEach(([r, c]) => {
-    const startX = c - 1;
-    const startY = r - 1;
     if (!visited[startY][startX]) {
-        totalReachable += bfs(startX, startY);
+        answer += bfs(startX, startY);
     }
-});
+}
 
-console.log(totalReachable);
+console.log(answer);
