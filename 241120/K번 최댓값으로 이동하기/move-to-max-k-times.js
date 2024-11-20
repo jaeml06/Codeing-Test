b329e59e945d6df13758480867aadae90ab45fde
jaeml06 <jaeml0630@gmail.com>
2024-11-20 13:47:03 +0900

    Title: K번 최댓값으로 이동하기, Time: 188ms, Memory: 15MB, Status: Passed - CodeTree

--- 241120/K번 최댓값으로 이동하기/move-to-max-k-times.js
+++ 241120/K번 최댓값으로 이동하기/move-to-max-k-times.js
@@ -1,56 +1,74 @@
 let fs = require('fs');
 let read = fs.readFileSync(0).toString().trim().split('\n');
-const [n, k] = read.splice(0, 1)[0].split(' ').map(Number)
+const [n, k] = read.splice(0, 1)[0].split(' ').map(Number);
 const graph = read.splice(0, n).map(val => val.split(' ').map(Number));
-const [r, c] = read.splice(0, 1)[0].split(' ').map(Number);
+const [startR, startC] = read.splice(0, 1)[0].split(' ').map(Number);
 
-const dx = [-1, 1, 0, 0]
-const dy = [0, 0, -1, 1]
-function searchTarget(x, y){
-    let max = Number.MIN_SAFE_INTEGER;
-    for(let i = 0; i< n; i++){
-        for(let j = 0; j < n; j++){
-            if(graph[y][x] > graph[i][j]){
-                max = Math.max(max, graph[i][j])
-            }
-        }
-    }
-    return max !== Number.MIN_SAFE_INTEGER ? max : null
-}
-function bfs(x, y, target, count){
-    const visited = Array.from({length: n}, () => Array(n).fill(false));
-    visited[y][x] = true
-    const queue = [[x, y]];
-    let head = 0;
+const dx = [-1, 1, 0, 0];
+const dy = [0, 0, -1, 1];
 
-    while(head < queue.length){
-        const [curX, curY] = queue[head++]
+let visited = [];
+let move = false;
+const max = { val: 0, r: startR - 1, c: startC - 1 }; 
+let q = [];
 
-        for(let i = 0; i < 4; i++){
-            const nextX = curX + dx[i];
-            const nextY = curY + dy[i];
-
-            if(0<=nextX && nextX < n && 0 <= nextY && nextY < n && !visited[nextY][nextX] && graph[nextY][nextX] < graph[r - 1][c- 1]){
-                if(graph[nextY][nextX] === target){
-                    if(count === k){
-                        return [nextX, nextY];
-                    }
-                    target = searchTarget(nextX, nextY)
-                    if (target === null) {
-                        return [nextX, nextY];
-                    }
-                    return bfs(nextX, nextY, target, count + 1)
-                } else{
-                    visited[nextY][nextX] = true
-                    queue.push([nextX, nextY]);
-                }
-            }
-        }
-    }
-
+function checkMax(nx, ny) {
+  if (graph[nx][ny] < max.val) return;
+  if (graph[nx][ny] > max.val) {
+    max.val = graph[nx][ny];
+    max.r = nx;
+    max.c = ny;
+    move = true;
+    return;
+  }
+  if (graph[nx][ny] === max.val && nx < max.r) {
+    max.r = nx;
+    max.c = ny;
+    move = true;
+    return;
+  }
+  if (graph[nx][ny] === max.val && nx === max.r && ny < max.c) {
+    max.c = ny;
+    move = true;
+    return;
+  }
 }
 
-const target = searchTarget(c- 1, r - 1)
+function bfs(maxVal) {
+  while (q.length) {
+    let [x, y] = q.shift();
+    for (let i = 0; i < 4; i++) {
+      let nx = x + dx[i];
+      let ny = y + dy[i];
 
-const answer = bfs(c - 1, r - 1, target,1);
-console.log(answer[1] + ' ' + answer[0])
+      if (
+        nx >= 0 &&
+        nx < n &&
+        ny >= 0 &&
+        ny < n &&
+        graph[nx][ny] < maxVal &&
+        !visited[nx][ny]
+      ) {
+        q.push([nx, ny]);
+        checkMax(nx, ny);
+        visited[nx][ny] = true;
+      }
+    }
+  }
+}
+
+for (let i = 0; i < k; i++) {
+  move = false;
+  let [r, c] = [max.r, max.c];
+  visited = Array.from({ length: n }, () =>
+    Array.from({ length: n }, () => false)
+  );
+
+  q.push([r, c]);
+  visited[r][c] = true;
+  bfs(graph[r][c]);
+  max.val = 0;
+  if (!move) break;
+}
+
+console.log(max.r + 1 + " " + (max.c + 1));
