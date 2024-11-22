3cc8f431ab05d4a2a90e2f0b865d5cae513a872c
jaeml06 <jaeml0630@gmail.com>
2024-11-22 13:01:14 +0900

    Title: 돌 잘 치우기, Time: 162ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241122/돌 잘 치우기/clear-stones-well.js
+++ 241122/돌 잘 치우기/clear-stones-well.js
@@ -4,7 +4,7 @@ const [n, k, m] = read.splice(0, 1)[0].split(' ').map(Number)
 
 const graph = read.splice(0, n).map(val => val.split(' ').map(Number));
 const startDots = read.splice(0, k).map(val => val.split(' ').map(Number));
-const visited = Array.from({length: n}, () => Array(n).fill(false));
+
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
 
@@ -17,10 +17,33 @@ graph.forEach((row, y) => {
     })
 })
 
+function combinations(arr, k) {
+    const result = [];
+
+    function helper(start, path) {
+        if (path.length === k) {
+            result.push([...path]);
+            return;
+        }
+
+        for (let i = start; i < arr.length; i++) {
+            path.push(arr[i]);
+            helper(i + 1, path);
+            path.pop();
+        }
+    }
+
+    helper(0, []);
+    return result;
+}
+const rockPosCombin = combinations(rockPos, m)
+
+
 function bfs(x, y){
+    const visited = Array.from({length: n}, () => Array(n).fill(false));
     visited[y][x] = true;
-    const queue = [[x, y]]
-    let head = 0
+    const queue = [[x, y]];
+    let head = 0;
 
     while(head < queue.length){
         const [curX, curY] = queue[head++];
@@ -29,19 +52,27 @@ function bfs(x, y){
             const nextY = curY + dy[i];
 
             if(0 <= nextX && nextX < n && 0 <= nextY && nextY < n && !visited[nextY][nextX] && graph[nextY][nextX] === 0){
-                visited[nextY][nextX] = true
-                queue.push([nextX, nextY])
+                visited[nextY][nextX] = true;
+                queue.push([nextX, nextY]);
             }
         }
 
     }
-    return head
+    return queue.length;
 
 }
-let maxCount = Number.MIN_SAFE_INTEGER
+let maxCount = Number.MIN_SAFE_INTEGER;
 startDots.forEach(([c, r]) => {
-    graph[r][c] = 0
-    maxCount = Math.max(bfs(c, r), maxCount)
+    rockPosCombin.forEach((rocks) => {
+        rocks.forEach(([x, y]) => {
+            graph[y][x] = 0
+        })
+         maxCount = Math.max(bfs(c, r), maxCount);
+         rocks.forEach(([x, y]) => {
+            graph[y][x] = 1
+        })
+    })
+   
 })
 
-console.log(maxCount + 1)
+console.log(maxCount)
