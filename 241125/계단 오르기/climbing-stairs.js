0f26e1a1c36066c9d9d4d9ddbe37970e4eb9cb86
jaeml06 <jaeml0630@gmail.com>
2024-11-25 19:02:40 +0900

    Title: 계단 오르기, Time: 176ms, Memory: 15MB, Status: Passed - CodeTree

--- 241125/계단 오르기/climbing-stairs.js
+++ 241125/계단 오르기/climbing-stairs.js
@@ -2,24 +2,21 @@ let fs = require('fs');
 let [temp1] = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(temp1);
 
-const memo = Array(1001).fill(-1);
+const memo = Array(1001).fill(0);
 
 function dp(n) {
-    if (n < 0) {
+    if (n <= 1) {
         return 0; 
     }
-    if (n === 0) {
-        return 1;
-    }
-    if (memo[n] !== -1) {
+    if (memo[n] !== 0) {
         return memo[n];
     }
     if (n === 2 || n === 3) {
         memo[n] = 1;
     } else {
-        memo[n] = dp(n - 2) + dp(n - 3);
+        memo[n] = (dp(n - 2) + dp(n - 3)) % 10007; 
     }
     return memo[n];
 }
 
-console.log(dp(n) % 10007);
+console.log(dp(n));
