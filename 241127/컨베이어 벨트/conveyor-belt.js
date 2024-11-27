0c17e3f47e4d76741f51e47b7430a5024db894b9
jaeml06 <jaeml0630@gmail.com>
2024-11-27 23:43:12 +0900

    Title: 컨베이어 벨트, Time: 200ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241127/컨베이어 벨트/conveyor-belt.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let [temp1, ...temp2] = fs.readFileSync(0).toString().trim().split('\n');
+const [n, t] = temp1.split(' ').map(Number);
+const container1 = temp2.splice(0, 1)[0].split(' ').map(Number)
+const container2 = temp2.splice(0, 1)[0].split(' ').map(Number)
+
+for(let i = 0; i < t; i++){
+    const box1 = container1.pop()
+    const box2 = container2.pop()
+
+    container1.unshift(box2)
+    container2.unshift(box1)
+}
+console.log(container1.join(' '))
+console.log(container2.join(' '))
