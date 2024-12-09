df93de5ee5a66ac8d73932b58704febc9d5feaa8
jaeml06 <jaeml0630@gmail.com>
2024-12-09 15:48:18 +0900

    Title: 최소 경로로 탈출 하기, Time: 184ms, Memory: 15MB, Status: Runtime Error - CodeTree

--- 241209/최소 경로로 탈출 하기/escape-with-min-distance.js
+++ 241209/최소 경로로 탈출 하기/escape-with-min-distance.js
@@ -1,8 +1,8 @@
 let fs = require('fs');
 let [temp1, ...temp2] = fs.readFileSync(0).toString().trim().split('\n');
 const [n, m] = temp1.split(' ').map(Number);
-const graph = temp2.map(val => val.trim().split(' ').map(Number));
-const visited = Array.from({length: m}, () => Array(n).fill(-1));
+const graph = temp2.map((val) => val.trim().split(' ').map(Number));
+const visited = Array.from({ length: m }, () => Array(n).fill(-1));
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
 
@@ -11,23 +11,34 @@ if (graph[0][0] === 0) {
     return;
 }
 
-function bfs(){
+function bfs() {
     const queue = [[0, 0]];
     visited[0][0] = 0;
-    let head = 0
-    while(head < queue.length){
-        const [curX, curY] = queue[head++]
-        for(let i = 0; i< 4; i++){
+    let head = 0;
+
+    while (head < queue.length) {
+        const [curX, curY] = queue[head++];
+        for (let i = 0; i < 4; i++) {
             const nexX = curX + dx[i];
             const nexY = curY + dy[i];
 
-            if(0 <= nexX && nexX < n && 0 <= nexY && nexY < m && visited[nexY][nexX] === -1 && graph[nexY][nexX] === 1){
+            if (
+                0 <= nexX && nexX < n &&
+                0 <= nexY && nexY < m &&
+                visited[nexY][nexX] === -1 &&
+                graph[nexY][nexX] === 1
+            ) {
                 queue.push([nexX, nexY]);
                 visited[nexY][nexX] = visited[curY][curX] + 1;
             }
         }
     }
+
+    if (visited[m - 1][n - 1] === -1) {
+        console.log(-1);
+    } else {
+        console.log(visited[m - 1][n - 1]);
+    }
 }
 
-bfs()
-console.log(visited[m-1][n-1])
+bfs();
