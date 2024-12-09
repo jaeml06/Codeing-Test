4cbecff0d45023510c1c2e5cd97233a555f309e6
jaeml06 <jaeml0630@gmail.com>
2024-12-09 15:55:16 +0900

    Title: 최소 경로로 탈출 하기, Time: 207ms, Memory: 16MB, Status: Passed - CodeTree

--- 241209/최소 경로로 탈출 하기/escape-with-min-distance.js
+++ 241209/최소 경로로 탈출 하기/escape-with-min-distance.js
@@ -2,14 +2,9 @@ let fs = require('fs');
 let [temp1, ...temp2] = fs.readFileSync(0).toString().trim().split('\n');
 const [n, m] = temp1.split(' ').map(Number);
 const graph = temp2.map((val) => val.trim().split(' ').map(Number));
-const visited = Array.from({ length: m }, () => Array(n).fill(-1));
+const visited = Array.from({ length: n }, () => Array(m).fill(-1));
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
-
-if (graph[0][0] === 0) {
-    console.log(-1);
-    return;
-}
 
 function bfs() {
     const queue = [[0, 0]];
@@ -23,8 +18,8 @@ function bfs() {
             const nexY = curY + dy[i];
 
             if (
-                0 <= nexX && nexX < n &&
-                0 <= nexY && nexY < m &&
+                0 <= nexX && nexX < m &&
+                0 <= nexY && nexY < n &&
                 visited[nexY][nexX] === -1 &&
                 graph[nexY][nexX] === 1
             ) {
@@ -34,11 +29,8 @@ function bfs() {
         }
     }
 
-    if (visited[m - 1][n - 1] === -1) {
-        console.log(-1);
-    } else {
-        console.log(visited[m - 1][n - 1]);
-    }
+
 }
 
 bfs();
+console.log(visited[n - 1][m - 1]);
