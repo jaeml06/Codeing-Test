125081d5df9ecb6cd1290a9040456f2a342f5fde
jaeml06 <jaeml0630@gmail.com>
2024-12-26 20:40:42 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 174ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241226/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.js
@@ -0,0 +1,20 @@
+let fs = require('fs');
+let [temp1] = fs.readFileSync(0).toString().trim().split('\n');
+
+const [K, N] = temp1.split(' ').map(Number);
+const answer = []
+
+function choose(n){
+    if(n === N){
+        console.log(answer.join(' '));
+        return;
+    }
+    for(let i = 1; i <= K; i++){
+        if(n >= 3 && answer[answer.length - 1] === answer[answer.length-2] && answer[answer.length - 1] === answer[answer.length-2]) continue;
+        answer.push(i);
+        choose(n+1)
+        answer.pop();
+    }
+}
+
+choose(0);
