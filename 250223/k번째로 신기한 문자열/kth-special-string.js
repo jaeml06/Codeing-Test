7c92d1fc3d2c583cd3d3bb757368cd48403c1d6a
jaeml06 <jaeml0630@gmail.com>
2025-02-23 18:15:22 +0900

    Title: k번째로 신기한 문자열, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

--- 250223/k번째로 신기한 문자열/kth-special-string.js
+++ 250223/k번째로 신기한 문자열/kth-special-string.js
@@ -3,7 +3,7 @@ let input = fs.readFileSync(0).toString().trim().split('\n').map(val => val.trim
 
 const [N, K, T] = input[0].split(' ')
 const arr = input.slice(1).reduce((prev, cur) => {
-    if(cur.includes(T)){
+    if(cur.slice(0, T.length) === T){
         prev.push(cur)
         return prev
     }
@@ -11,6 +11,5 @@ const arr = input.slice(1).reduce((prev, cur) => {
 }, [])
 
 arr.sort()
-
 console.log(arr[Number(K)- 1])
 
