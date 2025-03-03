a6006466d63dca86c36b963fa4091a1a07813b48
jaeml06 <jaeml0630@gmail.com>
2025-03-03 22:08:26 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 412ms, Memory: 62MB, Status: Wrong Answer - CodeTree

--- 250303/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
+++ 250303/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.js
@@ -6,15 +6,14 @@ const rect2 = input[1].split(' ').map(val => Number(val) + 1000);
 
 const grid = Array.from({length : 2001}, () => Array(2001).fill(false));
 
-for (let i = rect1[1]; i <= rect1[3]; i++) {
-  for (let j = rect1[0]; j <= rect1[2]; j++) {
+for (let i = rect1[1]; i < rect1[3]; i++) {
+  for (let j = rect1[0]; j < rect1[2]; j++) {
     grid[i][j] = true;
   }
 }
 
-// rect2
-for (let i = rect2[1]; i <= rect2[3]; i++) {
-  for (let j = rect2[0]; j <= rect2[2]; j++) {
+for (let i = rect2[1]; i < rect2[3]; i++) {
+  for (let j = rect2[0]; j < rect2[2]; j++) {
     grid[i][j] = false;
   }
 }
@@ -29,13 +28,12 @@ let minY = Number.MAX_SAFE_INTEGER
 grid.forEach((arr, indexY) => {
     arr.forEach((val, indexX) => {
         if(val){
-            maxX = Math.max(maxX, indexX + 1);
-            maxY = Math.max(maxY, indexY + 1);
-            minX = Math.min(minX, indexX + 1);
-            minY = Math.min(minY, indexY + 1);
+            maxX = Math.max(maxX, indexX);
+            maxY = Math.max(maxY, indexY);
+            minX = Math.min(minX, indexX);
+            minY = Math.min(minY, indexY);
 
         }
     })
 })
-
-console.log((maxX - minX) * (maxY - minY))
+console.log((maxX - minX + 1) * (maxY - minY + 1))
