acf63fbcbdb87951ef77c803daeb4ba540dd5fea
jaeml06 <jaeml0630@gmail.com>
2025-03-03 18:08:28 +0900

    Title: 색종이의 총 넓이, Time: 177ms, Memory: 15MB, Status: Runtime Error - CodeTree

---
+++ 250303/색종이의 총 넓이/the-total-area-of-colored-paper.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const rects = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+
+const grid = Array.from({length : 201}, () => {
+   return Array(201).fill(false)
+})
+
+rects.forEach(val => {
+    const [x, y] = val;
+    for(let i = y; i < y + 8; i++){
+        for(let j = x; j < x + 8; j++){
+            grid[i][j] = true;
+        }
+    }
+})
+
+let answer = 0
+
+grid.forEach(arr => {
+    arr.forEach(val => {
+        if(val){
+            answer+=1
+        }
+    })
+})
+
+console.log(answer)
