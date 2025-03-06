579e93f9760f54f583f03202b52abc9c60d9d00e
jaeml06 <jaeml0630@gmail.com>
2025-03-06 14:19:58 +0900

    Title: 연속되는 수 4, Time: 187ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250306/연속되는 수 4/continuous-number4.js
+++ 250306/연속되는 수 4/continuous-number4.js
@@ -3,12 +3,12 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input.slice(1, 1 + n).map(Number);
 
-let max = 0
+let max = 1
 let answer = 0
 
 arr.forEach((val, index) => {
     if(index === 0){
-        max++
+        
     } else if(arr[index - 1] < arr[index]){
         max++
     } else {
