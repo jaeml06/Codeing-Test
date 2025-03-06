65dd5022453d4e51d5b76a92e7d2d801f9d50022
jaeml06 <jaeml0630@gmail.com>
2025-03-06 14:09:06 +0900

    Title: 연속되는 수 3, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

--- 250306/연속되는 수 3/continuous-number3.js
+++ 250306/연속되는 수 3/continuous-number3.js
@@ -4,7 +4,20 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input.slice(1, 1 + n).map(Number);
 
-const minus = arr.filter(val => val < 0).length
-const plus = arr.filter(val => val > 0).length
+let maxPlus = 0, maxMinus = 0;
+let plus = 0, minus = 0;
 
-console.log(Math.max(minus, plus))
+arr.forEach(val => {
+    if (val > 0) {
+        plus += 1;
+        minus = 0;
+    } else if (val < 0) {
+        minus += 1;
+        plus = 0;
+    }
+
+    maxPlus = Math.max(maxPlus, plus);
+    maxMinus = Math.max(maxMinus, minus);
+});
+
+console.log(Math.max(maxPlus, maxMinus));
