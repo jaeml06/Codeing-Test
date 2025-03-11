a1cabde4580c931cdf950197808ea6b7ba2027c2
jaeml06 <jaeml0630@gmail.com>
2025-03-11 13:28:46 +0900

    Title: 문자에 따른 명령 2, Time: 182ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250311/문자에 따른 명령 2/text-based-commands2.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const commands = [...input[0]];
+
+const dx = [1, 0, -1, 0];
+const dy = [0, -1, 0, 1];
+
+let dirNum = 3;
+const pos = [0, 0]
+
+commands.forEach(command => {
+    if(command ==='L'){
+        dirNum = (dirNum - 1) % 4
+    }else if(command === 'R'){
+        dirNum = (dirNum + 1) % 4
+    }else if(command === 'F'){
+        pos[0] += dx[dirNum]
+        pos[1] += dy[dirNum]
+    }
+})
+
+console.log(pos.join(' '))
