69d091aa94c9b6233c9b1a363366d71f3470afb0
jaeml06 <jaeml0630@gmail.com>
2025-03-14 14:16:34 +0900

    Title: 되돌아오기, Time: 220ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250314/되돌아오기/come-back.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const moves = input.slice(1);
+
+const dir = {
+    'W': [-1, 0],
+    'E': [1, 0],
+    'S': [0, -1],
+    'N': [0, 1]
+};
+let x = 0;
+let y = 0;
+
+let answer = 0
+
+moves.some((val) => {
+    const [d, t] = val.split(' ');
+    for(let i = 0; i< Number(t); i++){
+        x += dir[d][0];
+        y += dir[d][1];
+        answer++
+        if(x === 0 && y===0){
+            return true
+        }
+    }
+})
+
+console.log(answer === 0 ? -1 : answer)
