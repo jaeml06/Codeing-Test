470e7059d513f833f1696c4e0f10aca3da6f70cc
jaeml06 <jaeml0630@gmail.com>
2025-03-20 14:10:34 +0900

    Title: 특정 구간의 원소 평균값, Time: 181ms, Memory: 16MB, Status: Passed - CodeTree

--- 250320/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.js
+++ 250320/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.js
@@ -1,19 +1,21 @@
 const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split('\n');
+const input = fs.readFileSync(0).toString().trim().split("\n");
 
 const n = Number(input[0]);
-const arr = input[1].trim().split(' ').map(Number);
+const arr = input[1].trim().split(" ").map(Number);
 
-// Please Write your code here.
-let count = 0
+let count = 0;
 
-for(let i = 0; i < n; i++){
-    for(let j = i; j < n; j++){
-        const temp = arr.slice(i, i=== j ? i + 1 : j + 2)
-        const avg = temp.reduce((prev, cur) => prev + cur, 0) / temp.length
-        if (avg % 1 === 0 && temp.includes(avg)) {
+for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+        const temp = arr.slice(i, j + 1);
+        const sum = temp.reduce((prev, cur) => prev + cur, 0);
+        const avg = sum / temp.length;
+
+        if (avg % 1 === 0 && temp.includes(avg)) { // ✅ 평균이 정수인지 확인 후 탐색
             count++;
         }
     }
 }
-console.log(count)
+
+console.log(count);
