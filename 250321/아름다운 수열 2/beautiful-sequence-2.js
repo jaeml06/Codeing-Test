3ab6a6bd24d7efc0df0089aa14b055d209df8b72
jaeml06 <jaeml0630@gmail.com>
2025-03-21 14:37:17 +0900

    Title: 아름다운 수열 2, Time: 3082ms, Memory: 160MB, Status: Memory Limit Exceed - CodeTree

--- 250321/아름다운 수열 2/beautiful-sequence-2.js
+++ 250321/아름다운 수열 2/beautiful-sequence-2.js
@@ -5,28 +5,30 @@ const [n, m] = input[0].split(" ").map(Number);
 const arr1 = input[1].split(" ").map(Number);
 const arr2 = input[2].split(" ").map(Number);
 
-const getPermutations = (arr) => {
-    if (arr.length === 1) return [arr];
-    
+const getPermutations = (arr, selectNum) => {
+    if (selectNum === 1) return arr.map((v) => [v]);
+
     let result = [];
     arr.forEach((fixed, index, origin) => {
         const rest = [...origin.slice(0, index), ...origin.slice(index + 1)];
-        const perms = getPermutations(rest);
-        const attached = perms.map(p => [fixed, ...p]);
+        const perms = getPermutations(rest, selectNum - 1);
+        const attached = perms.map((p) => [fixed, ...p]);
         result.push(...attached);
     });
+
     return result;
 };
 
-const bPermutations = new Set(getPermutations(arr2).map(p => p.join(",")));
+const bPermutations = getPermutations(arr2, m);
 
 let count = 0;
 
 for (let i = 0; i <= n - m; i++) {
-    const subArr = arr1.slice(i, i + m).join(",");
-    if (bPermutations.has(subArr)) {
-        count++;
-    }
+    const subArr = arr1.slice(i, i + m)
+    const flag  = bPermutations.some((val) => {
+        return val.every((a, b) => a === subArr[b])
+    })
+    if(flag) count++
 }
 
 console.log(count);
