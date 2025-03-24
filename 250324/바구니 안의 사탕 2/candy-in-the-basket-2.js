69c9f0cc2d127d23f5cbb490d8cfba099a6bdc07
jaeml06 <jaeml0630@gmail.com>
2025-03-24 22:50:50 +0900

    Title: 바구니 안의 사탕 2, Time: 187ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250324/바구니 안의 사탕 2/candy-in-the-basket-2.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+let index = 0;
+const [n, k] = input[index++].split(" ").map(Number);
+
+const arr = Array(101).fill(0);
+
+for (let i = 0; i < n; i++) {
+    const [candy, position] = input[index++].split(" ").map(Number);
+    arr[position] += candy
+}
+let answer = 0
+for(let i = 0; i < arr.length - (2*k); i++){
+    const temp = arr.slice(i, i+(2 * k) + 1);
+
+    const sum = temp.reduce((prev, cur) => {
+        return prev + cur;
+    }, 0);
+
+    answer = Math.max(sum, answer)
+}
+
+console.log(answer)
+
