ee9e81c0c0cb3b35a86a963800f5f5f37f092547
jaeml06 <jaeml0630@gmail.com>
2025-03-25 11:52:01 +0900

    Title: G or H 2, Time: 185ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250325/G or H 2/G-or-H-2.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input.shift());
+const people = Array(101).fill('E');
+for (let i = 0; i < n; i++) {
+    const [posStr, letter] = input[i].split(' ');
+    const pos = Number(posStr);
+    people[pos] = letter
+}
+let answer = 0
+for(let i = 0; i < people.length; i++){
+    let countG = 0;
+    let countH = 0;
+    let max = 0;
+    if(people[i] !== 'E'){
+        if(people[i] === 'G'){
+            countG++
+        }else {
+            countH++
+        }
+        for(let j = 0; j < people.length; j++){
+            if(people[j] !== 'E'){
+                if(people[j] === 'G'){
+                    countG++
+                }else {
+                    countH++
+                }
+                if((countG !== 0 && countH === 0) || (countG === 0 && countH !==0) || (countG === countH)){
+                    max = Math.max(max, j-i);
+                }
+            }
+
+            
+        }
+    }
+    answer = Math.max(answer, max)
+}
+
+console.log(answer)
+// Please Write your code here.
