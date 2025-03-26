12302ae158377d51be6960bf6b5349501b0f5009
jaeml06 <jaeml0630@gmail.com>
2025-03-26 15:24:00 +0900

    Title: 밭의 높이를 고르게하기, Time: 177ms, Memory: 15MB, Status: Passed - CodeTree

--- 250326/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
+++ 250326/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.js
@@ -6,11 +6,10 @@ const arr = input[1].split(" ").map(Number);
 
 let minValue = Number.MAX_SAFE_INTEGER;
 
-for (let i = 0; i < N - T + 1; i++) {
+for (let i = 0; i < n - t + 1; i++) {
   let temp = 0;
-  // j를 0부터 T-1까지 순회
-  for (let j = 0; j < T; j++) {
-    temp += Math.abs(H - arr[i + j]);
+  for (let j = 0; j < t; j++) {
+    temp += Math.abs(h - arr[i + j]);
   }
   if (temp < minValue) {
     minValue = temp;
