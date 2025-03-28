4861a5eefcdb8d308e7872279fb47e1cf3d2a8e8
jaeml06 <jaeml0630@gmail.com>
2025-03-28 13:35:54 +0900

    Title: 숫자 카운트, Time: 171ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250328/숫자 카운트/numeric-count.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+
+const map = new Map();
+arr.forEach(([text, count1, count2]) => {
+    for(let i = 1; i <= 9; i++){
+        for(let j = 1; j <= 9; j++){
+            for(let k = 1; k <= 9; k++){
+                let temp1 = 0;
+                let temp2 = 0;
+                const tempArr = [...text.toString()];
+                tempArr.forEach((val, index) => {
+                    if(index === 0){
+                        if(Number(val) === i){
+                            temp1++
+                        }
+                    } else{
+                        if(Number(val) === i){
+                            temp2++
+                        }
+                    }
+                });
+                tempArr.forEach((val, index) => {
+                    if(index === 1){
+                        if(Number(val) === j){
+                            temp1++
+                        }
+                    }else{
+                        if(Number(val) === j){
+                            temp2++
+                        }
+                    }
+                });
+                tempArr.forEach((val, index) => {
+                    if(index === 2){
+                        if(Number(val) === k){
+                            temp1++
+                        }
+                    }else{
+                        if(Number(val) === k){
+                            temp2++
+                        }
+                    }
+                });
+                if(temp1 === count1 && temp2 === count2){
+                    map.set(`${i}${j}${k}`, (map.get(`${i}${j}${k}`) ?? 0) + 1);
+                }
+            }
+        }
+    }
+})
+let count = 0
+map.forEach((val) =>{
+    if(val === n){
+        count++
+    }
+})
+console.log(count)
