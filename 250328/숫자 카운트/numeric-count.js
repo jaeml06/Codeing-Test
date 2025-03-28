31bdd7b303aa50d375416cb6553fcab27d984be8
jaeml06 <jaeml0630@gmail.com>
2025-03-28 13:41:47 +0900

    Title: 숫자 카운트, Time: 202ms, Memory: 17MB, Status: Passed - Codetree

--- 250328/숫자 카운트/numeric-count.js
+++ 250328/숫자 카운트/numeric-count.js
@@ -4,11 +4,13 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const arr = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
 
+
 const map = new Map();
 arr.forEach(([text, count1, count2]) => {
     for(let i = 1; i <= 9; i++){
         for(let j = 1; j <= 9; j++){
             for(let k = 1; k <= 9; k++){
+                if(i === j || j === k || i === k) continue;
                 let temp1 = 0;
                 let temp2 = 0;
                 const tempArr = [...text.toString()];
