2bdce0b216a8bd665536a525cdfb076f3821885d
jaeml06 <jaeml0630@gmail.com>
2025-05-06 14:35:21 +0900

    Title: 야바위, Time: 179ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250506/야바위/ya-rock.js
+++ 250506/야바위/ya-rock.js
@@ -3,22 +3,26 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const commands = input.slice(1).map(line => line.split(' ').map(Number));
 
-let max = 0
+let max = 0;
 
-for(let i = 1; i <= n; i++){
-            const arr = Array(n).fill(false);
-        arr[i - 1]=true;
-    let count = 0
-    for(let j = 0; j < 3; j++){
+for (let i = 1; i <= n; i++) {
+  const arr = Array(n).fill(false);
+  arr[i - 1] = true;
+  let count = 0;
 
+  for (let j = 0; j < 3; j++) {
+    const [a, b, guess] = commands[j];
 
-        const temp = arr[commands[j][1] - 1];
-        arr[commands[j][1] - 1] = arr[commands[j][0] - 1];
-        arr[commands[j][0] - 1] = temp;
-        if(arr[commands[j][2] - 1]){
-            count++
-        }
+    const temp = arr[a - 1];
+    arr[a - 1] = arr[b - 1];
+    arr[b - 1] = temp;
+
+    if (arr[guess - 1]) {
+      count++;
     }
-    max = Math.max(max, count)
+  }
+
+  max = Math.max(max, count);
 }
-console.log(max)
+
+console.log(max);
