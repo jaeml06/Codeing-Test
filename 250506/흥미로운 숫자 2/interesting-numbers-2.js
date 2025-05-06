64fef15ab483f0dcd48a8dc3b191788ed6698c15
jaeml06 <jaeml0630@gmail.com>
2025-05-06 14:13:10 +0900

    Title: 흥미로운 숫자 2, Time: 434ms, Memory: 33MB, Status: Passed - Codetree

--- 250506/흥미로운 숫자 2/interesting-numbers-2.js
+++ 250506/흥미로운 숫자 2/interesting-numbers-2.js
@@ -2,24 +2,22 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [x, y] = input[0].split(" ").map(Number);
-let count = 0
-for(let i = x; i <= y; i++){
-    const arr = [...i.toString()].map(Number);
-    const map = new Map();
-    arr.forEach((val) => {
-        map.set(val, map.has() ? map.get(val) + 1 : 0)
-    })
+let count = 0;
 
-if(map.size === 2){
-    let flag = false;
-    map.values(val => {
-        if(val !== 1 && map !== arr.length - 1){
-            flag = true;
-        }
-    })
-    if(!flag){
-        count++
+for (let i = x; i <= y; i++) {
+  const digits = [...i.toString()];
+  const map = new Map();
+
+  for (const digit of digits) {
+    map.set(digit, (map.get(digit) || 0) + 1);
+  }
+
+  if (map.size === 2) {
+    const freqs = [...map.values()];
+    if ((freqs.includes(1) && freqs.includes(digits.length - 1))) {
+      count++;
     }
+  }
 }
-}
-console.log(count)
+
+console.log(count);
