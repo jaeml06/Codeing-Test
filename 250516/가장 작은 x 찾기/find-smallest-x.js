c85bfe176e36dd06a287d3b4df96669442a8372c
jaeml06 <jaeml0630@gmail.com>
2025-05-16 14:00:48 +0900

    Title: 가장 작은 x 찾기, Time: 203ms, Memory: 15MB, Status: Passed - Codetree

--- 250516/가장 작은 x 찾기/find-smallest-x.js
+++ 250516/가장 작은 x 찾기/find-smallest-x.js
@@ -5,7 +5,7 @@ const n = Number(input[0]);
 const conditions = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 
 let answer = 0
-for(let i = 1; i <= 10; i++){
+for(let i = 1; i <= 10_000; i++){
     let temp = i
     const flag = conditions.every(val => {
         temp = temp * 2;
