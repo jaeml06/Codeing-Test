84dfc323e883a4c413dae48925dce1918da76924
jaeml06 <jaeml0630@gmail.com>
2025-06-28 15:51:22 +0900

    Title: 알파벳과 사칙연산, Time: 5015ms, Memory: 15MB, Status: Time Limit Exceed - Codetree

--- 250628/알파벳과 사칙연산/calculations-with-alphabet.js
+++ 250628/알파벳과 사칙연산/calculations-with-alphabet.js
@@ -2,45 +2,39 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const expression = input[0];
+const opers = [...expression].filter(val => val === '+' || val === '-' || val === '*');
 const count = [...expression].filter(val => val !== '+' && val !== '-' && val !== '*').length;
-const str = [...expression].filter(val => val === '+' || val === '-' || val === '*');
-function combin(n){
-    const result = []
 
-    function bfs(temp){
-        if(temp.length === n){
-           result.push([...temp])
-           return;
+let max = Number.MIN_SAFE_INTEGER;
+
+function dfs(path) {
+    if (path.length === count) {
+        let sum = path[0];
+        let j = 0;
+        for (let i = 1; i < path.length; i++) {
+            switch (opers[j++]) {
+                case '+':
+                    sum += path[i];
+                    break;
+                case '-':
+                    sum -= path[i];
+                    break;
+                case '*':
+                    sum *= path[i];
+                    break;
+            }
         }
-        for(let i = 1; i <= 4; i++){
-            temp.push(i);
-            bfs(temp);
-            temp.pop();
-        }
+        max = Math.max(max, sum);
+        return;
     }
-    bfs([]);
-    return result;
+
+    for (let i = 1; i <= 4; i++) {
+        path.push(i);
+        dfs(path);
+        path.pop();
+    }
 }
 
-let max = Number.MIN_SAFE_INTEGER
-const arr = combin(count);
-arr.forEach(val => {
-    let sum = val[0]
-    let j = 0;
-    for(let i = 1; i < val.length; i++){
-        switch(str[j++]){
-            case '+' :
-                sum += val[i]
-                break;
-            case '-' :
-                sum -= val[i]
-                break;
-            case '*' :
-                sum *= val[i]
-                break;
-        }
-    }
-    max = Math.max(max, sum)
-})
+dfs([]);
 
-console.log(max)
+console.log(max);
