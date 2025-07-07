c7d369bb146299285c1df3a6259ef741de6f200f
jaeml06 <jaeml0630@gmail.com>
2025-07-07 16:46:30 +0900

    Title: 1차원 젠가, Time: 203ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250707/1차원 젠가/jenga-1d.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const blocks = input.slice(1, n + 1).map(Number);
+const [s1, e1] = input[n + 1].split(' ').map(Number);
+const [s2, e2] = input[n + 2].split(' ').map(Number);
+
+blocks.splice(s1 - 1, e1 - s1 + 1);
+blocks.splice(s2-1, e2- s2 + 1);
+console.log(blocks.length)
+blocks.forEach(val => console.log(val))
