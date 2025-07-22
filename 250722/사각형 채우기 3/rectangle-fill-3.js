5c5646e12a7db045dfe893b86618e0ccbe9c8da8
jaeml06 <jaeml0630@gmail.com>
2025-07-22 13:27:45 +0900

    Title: 사각형 채우기 3, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

--- 250722/사각형 채우기 3/rectangle-fill-3.js
+++ 250722/사각형 채우기 3/rectangle-fill-3.js
@@ -2,13 +2,17 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-
+const mod = 1_000_000_007;
 const temp = Array(n + 1).fill(0);
 
+temp[0] = 1
 temp[1] = 2;
 temp[2] = 7;
 
-for(let i = 3; i <n+1; i++){
-    temp[i] = ((temp[i-2] * temp[i-1] * 2) - 6) % 1_000_000_007;
+for(let i = 3; i < n+1; i++){
+    temp[i] = ((3*temp[i-2]) + (2 * temp[i-1])) % mod;
+    for(let j = i - 3; j >= 0; j--){
+        temp[i] = (temp[i] + temp[j] * 2) % mod;
+    }
 }
 console.log(temp[n])
