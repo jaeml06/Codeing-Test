9d19d9bf79ba46dcc9b244b8ec702c92e1b68aa4
jaeml06 <jaeml0630@gmail.com>
2025-07-24 13:17:35 +0900

    Title: 정수 찾기, Time: 826ms, Memory: 40MB, Status: Wrong Answer - Codetree

--- 250724/정수 찾기/find-an-integer.js
+++ 250724/정수 찾기/find-an-integer.js
@@ -5,5 +5,7 @@ const n = Number(input[0]);
 const arr1 = input[1].trim().split(' ').map(Number);
 const m = Number(input[2]);
 const arr2 = input[3].trim().split(' ').map(Number);
+const temp1 = new Set(arr1);
+const temp2 = new Set(arr2);
 
-arr2.forEach(val => console.log(arr1.includes(val) ? 1: 0))
+[...temp2].forEach(val => console.log(temp1.has(val) ? 1: 0))
