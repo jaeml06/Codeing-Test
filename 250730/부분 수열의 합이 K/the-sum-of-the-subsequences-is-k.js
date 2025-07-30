01a2a51c9cb0b20e2dd5138f59dbd2cb1997d677
jaeml06 <jaeml0630@gmail.com>
2025-07-30 23:43:12 +0900

    Title: 부분 수열의 합이 K, Time: 175ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250730/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].trim().split(' ').map(Number);
+const arr = input[1].trim().split(' ').map(Number);
+
+const prefixSum = Array(n).fill(0);
+let count = 0;
+
+prefixSum[0] = 0;
+
+for(let i = 1; i < n; i++){
+    prefixSum[i] = prefixSum[i - 1] + arr[i];
+}
+
+for(let i = 0; i <= n; i++){
+    for(let j = i+1; j <= n; j++){
+        if(k === prefixSum[j] - prefixSum[i] + arr[i]){
+            count++
+        }
+    }
+}
+
+console.log(count)
