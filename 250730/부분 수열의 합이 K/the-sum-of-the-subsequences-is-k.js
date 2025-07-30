7e09399596f759114c270e992c0358c20026a705
jaeml06 <jaeml0630@gmail.com>
2025-07-30 23:44:06 +0900

    Title: 부분 수열의 합이 K, Time: 188ms, Memory: 15MB, Status: Passed - Codetree

--- 250730/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.js
+++ 250730/부분 수열의 합이 K/the-sum-of-the-subsequences-is-k.js
@@ -13,7 +13,7 @@ for(let i = 1; i < n; i++){
 }
 
 for(let i = 0; i <= n; i++){
-    for(let j = i+1; j <= n; j++){
+    for(let j = i; j <= n; j++){
         if(k === prefixSum[j] - prefixSum[i] + arr[i]){
             count++
         }
