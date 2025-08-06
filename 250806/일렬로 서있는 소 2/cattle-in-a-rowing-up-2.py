510e4f82e216e0f6858b664384f2264cfc8a979c
jaeml06 <jaeml0630@gmail.com>
2025-08-06 23:00:59 +0900

    Title: 일렬로 서있는 소 2, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250806/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -0,0 +1,13 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+
+count = 0
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if arr[i] <= arr[j] and arr[j] <= arr[n]:
+                count += 1
+
+print(count)
