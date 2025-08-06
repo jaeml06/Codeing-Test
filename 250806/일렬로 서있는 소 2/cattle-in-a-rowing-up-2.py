c308c437a97648baac981b9283eac9c1443d6a05
jaeml06 <jaeml0630@gmail.com>
2025-08-06 23:02:03 +0900

    Title: 일렬로 서있는 소 2, Time: 53ms, Memory: 17MB, Status: Passed - Codetree

--- 250806/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 250806/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -1,13 +1,11 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-# Please write your code here.
-
 count = 0
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
-            if arr[i] <= arr[j] and arr[j] <= arr[n]:
+            if arr[i] <= arr[j] and arr[j] <= arr[k]:
                 count += 1
 
 print(count)
