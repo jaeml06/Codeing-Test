7d585bccbea22175948d65b2d1f35972514f542e
jaeml06 <jaeml0630@gmail.com>
2025-08-10 16:01:03 +0900

    Title: 개발자의 순위, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250810/개발자의 순위/developers-rank.py
@@ -0,0 +1,19 @@
+k, n = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(k)]
+
+# Please write your code here.
+
+temp = {}
+for i in range(k):
+    for j in range(n):
+        for l in range(j + 1, n):
+            t1 = arr[i][j]
+            t2 = arr[i][l]
+            temp[f"{t1}{t2}"] = temp[f"{t1}{t2}"] + 1 if temp.get(f"{t1}{t2}") else 1
+
+cnt = 0
+for i in temp.values():
+    if i == k:
+        cnt +=1
+            
+print(cnt)
