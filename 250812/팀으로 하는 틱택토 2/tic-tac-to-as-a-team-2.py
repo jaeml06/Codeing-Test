d3c8daa7f62039629acc62d92f4294e19477f178
jaeml06 <jaeml0630@gmail.com>
2025-08-12 14:55:54 +0900

    Title: 팀으로 하는 틱택토 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250812/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,23 @@
+inp = [list(map(int, input())) for _ in range(3)]
+
+# Please write your code here.
+cnt = 0
+for team1 in range(1, 10):
+    for team2 in range(team1 + 1, 10):
+        if team1 == team2:
+            continue
+        for i in range(3):
+            temp = set(inp[i])
+            if len(temp) == 2 and team1 in temp and team2 in temp:
+                cnt+=1
+        for a in zip(inp[0], inp[1], inp[2]):
+            temp = set(a)
+            if len(temp) == 2 and team1 in temp and team2 in temp:
+                cnt+=1
+        temp = set([inp[0][0], inp[1][1], inp[2][2]])
+        if len(temp) == 2 and team1 in temp and team2 in temp:
+                cnt+=1
+        temp = set([inp[2][0], inp[1][1], inp[0][2]])
+        if len(temp) == 2 and team1 in temp and team2 in temp:
+                cnt+=1
+print(cnt)
