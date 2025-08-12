5d23e191e0f60d03d13306cea7999d10967ac113
jaeml06 <jaeml0630@gmail.com>
2025-08-12 15:00:24 +0900

    Title: 팀으로 하는 틱택토 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250812/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 250812/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -6,18 +6,20 @@ for team1 in range(1, 10):
     for team2 in range(team1 + 1, 10):
         if team1 == team2:
             continue
+        flag = False
         for i in range(3):
             temp = set(inp[i])
             if len(temp) == 2 and team1 in temp and team2 in temp:
-                cnt+=1
+                flag = True
         for a in zip(inp[0], inp[1], inp[2]):
             temp = set(a)
             if len(temp) == 2 and team1 in temp and team2 in temp:
-                cnt+=1
+                flag = True
         temp = set([inp[0][0], inp[1][1], inp[2][2]])
         if len(temp) == 2 and team1 in temp and team2 in temp:
-                cnt+=1
+                flag = True
         temp = set([inp[2][0], inp[1][1], inp[0][2]])
         if len(temp) == 2 and team1 in temp and team2 in temp:
-                cnt+=1
+                flag = True
+        if flag: cnt+=1
 print(cnt)
