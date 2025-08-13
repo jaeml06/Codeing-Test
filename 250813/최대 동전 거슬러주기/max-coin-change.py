5857a95097c8364093e00ed9b876a34840387c52
jaeml06 <jaeml0630@gmail.com>
2025-08-13 14:24:16 +0900

    Title: 최대 동전 거슬러주기, Time: 57ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250813/최대 동전 거슬러주기/max-coin-change.py
+++ 250813/최대 동전 거슬러주기/max-coin-change.py
@@ -4,11 +4,12 @@ coin = list(map(int, input().split()))
 # Please write your code here.
 dp = [0] * (M + 1)
 for i in coin:
-    dp[i] = 1
+    if i <= M:
+        dp[i] = 1
 
 for i in range(1, M + 1):
     for j in coin:
         if i - j < 1:
             continue
         dp[i] = max(dp[i], dp[i- j] + 1)
-print(dp[M])
+print(dp[M] if dp[M] != 0 else -1)
