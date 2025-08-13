8071f1a734799f592aba3a08abe563f0759f91e8
jaeml06 <jaeml0630@gmail.com>
2025-08-13 14:33:31 +0900

    Title: 최대 동전 거슬러주기, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 250813/최대 동전 거슬러주기/max-coin-change.py
+++ 250813/최대 동전 거슬러주기/max-coin-change.py
@@ -10,4 +10,4 @@ for i in range(1, M + 1):
     for j in coin:
         if i - j >= 0 and dp[i - j] != -sys.maxsize:
             dp[i] = max(dp[i], dp[i- j] + 1)
-print(dp[M] if dp[M] != 0 else -1)
+print(dp[M] if dp[M] != -sys.maxsize else -1)
