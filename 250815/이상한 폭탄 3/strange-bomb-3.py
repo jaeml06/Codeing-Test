76258b349abd7c0bc301af2c86c9c291b60edb3a
jaeml06 <jaeml0630@gmail.com>
2025-08-15 12:42:55 +0900

    Title: 이상한 폭탄 3, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250815/이상한 폭탄 3/strange-bomb-3.py
@@ -0,0 +1,18 @@
+N, K = map(int, input().split())
+num = [int(input()) for _ in range(N)]
+
+# Please write your code here.
+max_cnt = 0
+for i in range(N):
+    target = num[i]
+    idx1 = i
+    idx2 = i
+    cnt =0
+    while idx2 < N-1:
+        idx2+=1
+        if num[idx2] == target:
+            if K >= idx2 - idx1:
+                cnt+=1
+    max_cnt = max(max_cnt, cnt)
+print(max_cnt)
+
