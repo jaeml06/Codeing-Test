c416efd71c666e0f0b4c616e26c4543b671de276
jaeml06 <jaeml0630@gmail.com>
2025-08-16 16:18:06 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 150ms, Memory: 26MB, Status: Passed - Codetree

--- 250816/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
+++ 250816/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
@@ -1,15 +1,7 @@
-# 입력: K N
 K, N = map(int, input().split())
 
-path = []
-
-def dfs(depth):
-    if depth == N:
-        print(*path)
-        return
-    for x in range(1, K + 1):
-        path.append(x)
-        dfs(depth + 1)
-        path.pop()
-
-dfs(0)
+# Please write your code here.
+from itertools import product
+arr = list(range(1, K + 1))
+for i in product(arr, repeat=N):
+    print(*i)
