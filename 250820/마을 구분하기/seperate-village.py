b690f13237dcb0934e0e3b1f51173bafbe7438ea
jaeml06 <jaeml0630@gmail.com>
2025-08-20 20:32:09 +0900

    Title: 마을 구분하기, Time: 51ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250820/마을 구분하기/seperate-village.py
@@ -0,0 +1,32 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [[False] * n for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+cnt = 0
+def dfs(x, y):
+    global cnt
+    for i in range(4):
+        nx = dx[i] + x
+        ny = dy[i] + y
+
+        if 0 <= nx and nx < n and 0 <= ny and ny < n and grid[ny][nx] == 1 and not visited[ny][nx]:
+            visited[ny][nx] = True
+            cnt += 1
+            dfs(nx, ny)
+ans = []
+for i in range(n):
+    for j in range(n):
+        if grid[i][j] == 1 and not visited[i][j]:
+            cnt = 1
+            visited[i][j] = True
+            dfs(j, i)
+            ans.append(cnt)
+            
+ans.sort()
+print(len(ans))
+for val in ans:
+    print(val)
