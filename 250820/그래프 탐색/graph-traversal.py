206ae92481f61fdebba162a5aebe9987a2ae15ae
jaeml06 <jaeml0630@gmail.com>
2025-08-20 19:38:04 +0900

    Title: 그래프 탐색, Time: 98ms, Memory: 20MB, Status: Passed - Codetree

--- 250820/그래프 탐색/graph-traversal.py
+++ 250820/그래프 탐색/graph-traversal.py
@@ -2,21 +2,21 @@ n, m = map(int, input().split())
 edges = [tuple(map(int, input().split())) for _ in range(m)]
 
 # Please write your code here.
-graph = [[0 for _ in range(n+1)] for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
 
 visited = [False for _ in range(n+1)]
 
 count = 0
 
 for v1, v2 in edges:
-    graph[v1][v2] = 1
-    graph[v2][v1] = 1
+    graph[v1].append(v2)
+    graph[v2].append(v1)
 
 def dfs(v):
     global count
 
-    for cur in range(1, n+1):
-        if graph[v][cur] and not visited[cur]:
+    for cur in graph[v]:
+        if not visited[cur]:
             visited[cur] = True
             count += 1
             dfs(cur)
