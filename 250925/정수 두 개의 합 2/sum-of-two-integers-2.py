c8fda0a1b57a4bfacd6f4ba49f79e22d1d1d2305
jaeml06 <jaeml0630@gmail.com>
2025-09-25 22:03:10 +0900

    Title: 정수 두 개의 합 2, Time: 112ms, Memory: 18MB, Status: Passed - Codetree

--- 250925/정수 두 개의 합 2/sum-of-two-integers-2.py
+++ 250925/정수 두 개의 합 2/sum-of-two-integers-2.py
@@ -3,13 +3,16 @@ arr = [int(input()) for _ in range(n)]
 arr.sort()
 
 cnt = 0
-left, right = 0, n - 1
 
-while left < right:
-    if arr[left] + arr[right] <= k:
-        cnt += (right - left)
-        left += 1
-    else:
-        right -= 1
+right = n -1
+
+for left in range(n):
+    while arr[left] + arr[right] > k:
+        right -=1
+
+    if right <= left:
+        break
+    
+    cnt += right - left
 
 print(cnt)
