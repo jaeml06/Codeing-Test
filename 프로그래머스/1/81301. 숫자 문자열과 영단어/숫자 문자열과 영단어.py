2d213919ee71a8e2dae8cd71443f3aeef731714b
jaeml06 <jaeml0630@gmail.com>
2025-11-06 13:40:01 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,27 @@
+def solution(s):
+    num = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
+    }
+
+    answer = ''
+    temp = ''
+    
+    for ch in s:
+        if ch.isalpha():
+            temp += ch
+            if temp in num: 
+                answer += num[temp]
+                temp = ''
+        else:
+            answer += ch
+    
+    return int(answer)
