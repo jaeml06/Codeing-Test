9d1d5ad54a2b8d491267f6665a3fe7206042ce78
jaeml06 <jaeml0630@gmail.com>
2025-11-09 15:13:08 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,7 @@
+def solution(n, words):
+    for idx, word in enumerate(words):
+        if idx == 0:
+            continue
+        if word in words[:idx] or words[idx-1][-1] != word[0]:
+            return [(idx % n) + 1, (idx // n) + 1]
+    return [0, 0]
