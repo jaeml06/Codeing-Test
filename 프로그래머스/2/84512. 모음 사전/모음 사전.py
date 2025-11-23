2baba6d4b8aea4dbe79615bce0a0855b9910ed5c
jaeml06 <jaeml0630@gmail.com>
2025-11-23 18:10:21 +0900

    [level 2] Title: 모음 사전, Time: 1.54 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -0,0 +1,11 @@
+from itertools import product
+def solution(word):
+    answer = 0
+    a = ['A', 'E', 'I', 'O', 'U']
+    temp = []
+    for i in range(1, 6):
+        for p in product(a, repeat=i):
+            temp.append(''.join(p))
+    temp.sort()
+    answer = temp.index(word) + 1
+    return answer
