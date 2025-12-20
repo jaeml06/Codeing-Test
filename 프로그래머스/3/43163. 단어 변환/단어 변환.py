e8fe970913d86a6c2eb13ed422033be93b9f5b42
jaeml06 <jaeml0630@gmail.com>
2025-12-20 10:36:39 +0900

    [level 3] Title: 단어 변환, Time: 0.35 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -2,29 +2,25 @@ from collections import deque
 def solution(begin, target, words):
     if target not in words:
         return 0
-    
-    def can_change(a, b):
-        diff = 0
-        for x, y in zip(a, b):
-            if x != y:
-                diff += 1
-                if diff > 1:
-                    return False
-        return diff == 1
-
-    
-    visited = set()
+    v = set()
     q = deque([(begin, 0)])
+    v.add(begin)
+    
+    def can_go(w, word):
+        temp = 0
+        for x1, x2 in zip(w, word):
+            if x1 != x2:
+                temp += 1
+        return temp == 1
     
     while q:
-        word, cnt = q.popleft()
-        if word == target:
+        w, cnt = q.popleft()
+        if w == target:
             return cnt
-        for w in words:
-            if w not in visited and can_change(word, w):
-                visited.add(w)
-                q.append((w, cnt + 1))
-                
-    return 0
+        for word in words:
+            if word not in v and can_go(w, word):
+                v.add(word)
+                q.append((word, cnt+1))
+            
     
-    return answer
+    return 0
