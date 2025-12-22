16d9c8868cb22eba28800a6be374df2fd9a7fb4b
jaeml06 <jaeml0630@gmail.com>
2025-12-22 14:50:49 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 219ms, Memory: 27MB, Status: Passed - Codetree

---
+++ 251222/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
@@ -0,0 +1,18 @@
+import sys
+sys.setrecursionlimit(10**7)
+K, N = map(int, input().split())
+
+# Please write your code here.
+temp = []
+def permutations(cnt):
+    if cnt == N:
+        for num in temp:
+            print(num, end=' ')
+        print()
+        return
+    for i in range(1, K + 1):
+        temp.append(i)
+        permutations(cnt + 1)
+        temp.pop()
+permutations(0)
+
