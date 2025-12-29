f3d26cb71027fcb5102b7deb9bc68b5d5baf8ed4
JAEMIN LEE <jaeml0630@gmail.com>
2025-12-29 12:12:24 +0900

    [level 2] Title: 2 x n 타일링, Time: 10.90 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,12 @@
+def solution(n):
+    MOD = 1_000_000_007
+    dp = [0] * (n + 1)
+
+    dp[0] = 1
+    if n >= 1:
+        dp[1] = 1
+
+    for i in range(2, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD
+
+    return dp[n]
