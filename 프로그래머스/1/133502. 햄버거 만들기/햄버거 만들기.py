5e49deb659bcd7993b564da8077abf67e27f6133
JAEMIN LEE <jaeml0630@gmail.com>
2026-01-02 16:26:17 +0900

    [level 1] Title: 햄버거 만들기, Time: 304.90 ms, Memory: 25.6 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,10 +1,9 @@
-from collections import deque
 def solution(ingredient):
     answer = 0
     barger = []
     for i in ingredient:
         barger.append(i)
-        if barger[-4:] == [1,2,3,1]:
+        if barger[-4:] == [1, 2, 3, 1]:
+            answer+=1
             del barger[-4:]
-            answer +=1
     return answer
