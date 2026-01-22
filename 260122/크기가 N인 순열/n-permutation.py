9df117205edcaa8d449f41881e3da9d93073c08b
JAEMIN LEE <jaeml0630@gmail.com>
2026-01-22 15:14:24 +0900

    Title: 크기가 N인 순열, Time: 171ms, Memory: 27MB, Status: Passed - Codetree

---
+++ 260122/크기가 N인 순열/n-permutation.py
@@ -0,0 +1,24 @@
+n = int(input())
+visited = [False] * (n + 1)
+picked = []
+
+def dfs(cnt):
+    if cnt == n:
+        for v in picked:
+            print(v, end=' ')
+        print()
+        return
+    
+    for i in range(1, n + 1):
+        if visited[i]:
+            continue
+
+        visited[i] = True
+        picked.append(i)
+
+        dfs(cnt + 1)
+
+        visited[i] = False
+        picked.pop()
+
+dfs(0)
