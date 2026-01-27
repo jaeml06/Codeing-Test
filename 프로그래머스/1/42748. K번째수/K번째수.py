fccb2fef1338ab685a66bf56ebb01280b5cb5d4e
JAEMIN LEE <jaeml0630@gmail.com>
2026-01-27 16:52:37 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,6 @@
 def solution(array, commands):
     answer = []
-    for i, j , k in commands:
-        temp = sorted(array[i-1: j])[k - 1]
+    for i, j, k in commands:
+        temp = sorted(array[i - 1: j])[k-1]
         answer.append(temp)
     return answer
