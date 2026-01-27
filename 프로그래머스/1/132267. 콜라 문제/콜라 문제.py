5bd4af3fdcbd1ab1387a4f1f8ebc16cb0cd2d30c
JAEMIN LEE <jaeml0630@gmail.com>
2026-01-27 20:14:21 +0900

    [level 1] Title: 콜라 문제, Time: 0.37 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,8 +1,15 @@
 def solution(a, b, n):
     answer = 0
     while n >= a:
-        exchanged = (n//a) * b
-        answer += exchanged
+        e = (n // a) * b
+        answer += e
         
-        n = (n % a) + exchanged
+        n = (n % a) + e
+    return answer
+#     answer = 0
+#     while n >= a:
+#         exchanged = (n//a) * b
+#         answer += exchanged
+        
+#         n = (n % a) + exchanged
     return answer
