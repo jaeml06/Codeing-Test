db49d34e827222e25f3f23a69e97ba45bacd3311
JAEMIN LEE <jaeml0630@gmail.com>
2026-01-28 18:40:36 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,11 +1,9 @@
 def solution(n):
-    ans = 0
-    
+    answer = 0
     while n > 0:
         if n % 2 == 0:
             n //= 2
         else:
-            n -=1
-            ans +=1
-
-    return ans
+            n -= 1
+            answer += 1
+    return answer
