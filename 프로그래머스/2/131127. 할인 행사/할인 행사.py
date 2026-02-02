ad490c8c2d383a69592937a6732e0017ba2b8528
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-02 14:35:46 +0900

    [level 2] Title: 할인 행사, Time: 124.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,14 +1,19 @@
 from collections import Counter
 def solution(want, number, discount):
     answer = 0
-    need = {w: num for w, num in zip(want, number)}
-    total_days = 10
+    need = {w : num for w, num in zip(want, number)}
+    total_day = 10
     
-    if len(discount) < total_days:
+    if len(discount) < total_day:
         return 0
-    for i in range(0, len(discount) - total_days + 1):
-        window = Counter(discount[i:i + total_days])
-        if need == window:
-            answer+=1
+    for i in range(0, len(discount) - total_day + 1):
+        bound = {}
+        for val in discount[i:i + total_day]:
+            if val not in bound:
+                bound[val] = 1
+            else:
+                bound[val] += 1
+            if need == bound:
+                answer += 1
             
     return answer
