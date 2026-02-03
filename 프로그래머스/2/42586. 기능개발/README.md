6a15df2b1ec955a1a23aae43d89e29a750123a72
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-03 16:24:57 +0900

    [level 2] Title: 기능개발, Time: 0.11 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 9.21 MB, 시간: 0.07 ms
+메모리: 9.29 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 19일 14:32:45
+2026년 02월 03일 16:24:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,22 +1,22 @@
 def solution(progresses, speeds):
     days = []
     answer = []
-    for progress, speed in zip(progresses, speeds):
+    for p, s in zip(progresses, speeds):
         day = 0
-        total = progress
+        total = p
         while total < 100:
-            total += speed
+            total += s
             day += 1
         days.append(day)
-        
-    cur = days[0]
+    cur_day = days[0]
     cnt = 1
     for day in days[1:]:
-        if day <= cur:
+        if day <= cur_day:
             cnt += 1
         else:
             answer.append(cnt)
             cnt = 1
-            cur = day
+            cur_day = day
     answer.append(cnt)
+    
     return answer
