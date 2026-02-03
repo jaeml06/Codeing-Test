21fbde43263a85e65c00dc8a37d8b36ee1d3f750
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-03 16:03:53 +0900

    [level 2] Title: 피로도, Time: 38.92 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,17 +1,20 @@
-from itertools import permutations
 def solution(k, dungeons):
-    answer = -1
-    arr = permutations(dungeons, len(dungeons))
-    max_count = 0
-    for a in arr:
-        temp = k
-        count = 0
-        for need, consume in a:
-            if temp >= need:
-                temp -= consume
-                count += 1
-            else:
-                break
-        max_count = max(max_count, count)
-            
-    return max_count
+    n = len(dungeons)
+    visited = [False] * n
+    ans = 0
+    
+    def dfs(cur, cnt):
+        nonlocal ans
+        ans = max(ans, cnt)
+        
+        for i in range(n):
+            if visited[i]:
+                continue
+            need_s, consumed_s = dungeons[i]
+            if cur >= need_s:
+                visited[i] = True
+                dfs(cur - consumed_s, cnt + 1)
+                visited[i] = False
+    
+    dfs(k, 0)
+    return ans
