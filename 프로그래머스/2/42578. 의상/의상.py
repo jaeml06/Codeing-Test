10cdcd5282181790c3ce377e8aa32b0e982e1ade
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-03 15:30:00 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,12 +1,13 @@
 def solution(clothes):
-    temp = {}
-    for val, key in clothes:
-        if key not in temp:
-            temp[key] = []
-        temp[key].append(val)
+    product = {}
+    for v, k in clothes:
+        if k not in product:
+            product[k] = []
+        product[k].append(v)
     answer = 1
     
-    for key in temp:
-        answer *= (len(temp[key]) + 1)  # 입지 않는 선택 +1
-
+    for k in product:
+        answer *= (len(product[k]) + 1)
+        
     return answer - 1
+    
