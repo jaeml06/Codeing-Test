792989672d746b0adb3b02ce1c215b6a9a0faf00
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-03 15:11:51 +0900

    [level 1] Title: 기사단원의 무기, Time: 879.03 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/README.md
+++ 프로그래머스/1/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
 
 ### 성능 요약
 
-메모리: 9.22 MB, 시간: 877.94 ms
+메모리: 9.22 MB, 시간: 879.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 12일 21:48:44
+2026년 02월 03일 15:11:48
 
 ### 문제 설명
 
--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,16 +1,15 @@
 import math
 
 def solution(number, limit, power):
-    def count_divisors(n):
+    def count_d(n):
         cnt = 0
-        for i in range(1, int(math.sqrt(n)) + 1):
+        for i in range(1, math.isqrt(n) + 1):
             if n % i == 0:
                 cnt += 1 if i * i == n else 2
         return cnt
-
     answer = 0
-    for n in range(1, number + 1):
-        cnt = count_divisors(n)
+    for i in range(1, number + 1):
+        cnt = count_d(i)
         if cnt > limit:
             cnt = power
         answer += cnt
