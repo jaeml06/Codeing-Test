de588283a1136f62ccf65d315ced048f072078f8
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-04 12:07:46 +0900

    [level 1] Title: 모의고사, Time: 4.82 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 9.35 MB, 시간: 4.89 ms
+메모리: 9.37 MB, 시간: 4.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 14일 19:04:44
+2026년 02월 04일 12:07:43
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,14 +1,15 @@
 def solution(answers):
     answer = []
-    supo = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
+    supo_randoms = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
     count = [0, 0, 0]
-    for idx1, ans in enumerate(answers):
-        for idx2, val in enumerate(supo) :
-            if ans == val[idx1 % len(val)]:
-                count[idx2] += 1
-        
+    
+    for i1, ans in enumerate(answers):
+        for i2, supo_random in enumerate(supo_randoms):
+            if ans == supo_random[i1 % len(supo_random)]:
+                count[i2] += 1
     max_count = max(count)
-    for idx, val in enumerate(count):
-        if max_count == val:
+    for idx, c in enumerate(count):
+        if max_count == c:
             answer.append(idx + 1)
     return answer
+    
