4aba821fc8b120760368a950c0129c62bdae2d37
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-04 12:13:27 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -2,11 +2,10 @@ def solution(a, b):
     answer = ''
     months = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     days = ['SUN','MON','TUE','WED','THU','FRI','SAT']
-    start = 5
-    target = start
+    target = 5
     for i in range(a):
         target += months[i]
     target += b - 1
-    answer = days[(target%7)]
+    answer = days[(target % 7)]
         
     return answer
