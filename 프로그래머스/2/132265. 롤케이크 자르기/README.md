b9ace36aabf1cdfac883fa116c67ac91ed682028
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-05 15:21:30 +0900

    [level 2] Title: 롤케이크 자르기, Time: 563.13 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
 
 ### 성능 요약
 
-메모리: 125 MB, 시간: 515.86 ms
+메모리: 124 MB, 시간: 563.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 18일 15:52:28
+2026년 02월 05일 15:21:27
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,18 +1,20 @@
 def solution(topping):
     answer = 0
+    n = len(topping)
     left = [0] * len(topping)
     right = [0] * len(topping)
     left_set = set()
     right_set = set()
     
-    for i in range(len(topping)):
+    for i in range(n):
         left_set.add(topping[i])
         left[i] = len(left_set)
-    for i in range(len(topping)-1, -1, -1):
+    for i in range(n-1, -1, -1):
         right_set.add(topping[i])
         right[i] = len(right_set)
+    
         
-    for i in range(0, len(topping)-1):
+    for i in range(0, n-1):
         if left[i] == right[i+1]:
             answer+=1
     return answer
