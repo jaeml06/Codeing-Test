40b7624b014fa8568af8ec885f83a03fb905e85c
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-06 22:57:25 +0900

    Title: 1차원 젠가, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260206/1차원 젠가/jenga-1d.py
@@ -0,0 +1,11 @@
+n = int(input())
+blocks = [int(input()) for _ in range(n)]
+s1, e1 = map(int, input().split())
+s2, e2 = map(int, input().split())
+
+# Please write your code here.
+blocks = blocks[:s1-1] + blocks[e1:]
+blocks = blocks[:s2-1] + blocks[e2:]
+print(len(blocks))
+for b in blocks:
+    print(b)
