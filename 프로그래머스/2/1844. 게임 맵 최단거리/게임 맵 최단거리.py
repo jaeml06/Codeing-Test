9988647fa88c2cdab6c3c051d70b13d69b83db47
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-09 14:01:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.64 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -3,7 +3,7 @@ def solution(maps):
     answer = 0
     m = len(maps[0])
     n = len(maps)
-    visited = [[False] * m  for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     
@@ -12,17 +12,17 @@ def solution(maps):
         visited[y][x] = True
         
         while q:
-            cx, cy, count = q.popleft()
+            cx, cy, cnt = q.popleft()
             for i in range(4):
                 nx = dx[i] + cx
                 ny = dy[i] + cy
-                if 0 <= nx and nx < m and 0 <= ny and ny < n and not visited[ny][nx] and maps[ny][nx] == 1:
+                if 0 <= nx < m and 0<= ny < n and not visited[ny][nx] and maps[ny][nx] == 1:
                     visited[ny][nx] = True
-                    q.append((nx, ny, count + 1))
-                    if nx == m - 1 and ny == n - 1:
-                        return count + 1
+                    q.append((nx, ny, cnt + 1))
+                    if nx == m-1 and ny == n - 1:
+                        return cnt +1
+                    
         return -1
-    
-
     answer = bfs(0, 0)
+        
     return answer
