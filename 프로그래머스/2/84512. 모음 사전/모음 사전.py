991fbc054aeffecc780553e7f7dfeb1f9cd7a260
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-09 14:45:41 +0900

    [level 2] Title: 모음 사전, Time: 6.93 ms, Memory: 9.58 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,10 +1,23 @@
-from itertools import product
+def product(arr, repeat):
+    result = []
+    path = []
+    def dfs(depth):
+        if depth == repeat:
+            result.append(tuple(path))
+            return
+        for x in arr:
+            path.append(x)
+            dfs(depth + 1)
+            path.pop()
+    dfs(0)
+    return result
+
 def solution(word):
     answer = 0
     a = ['A', 'E', 'I', 'O', 'U']
     temp = []
     for i in range(1, 6):
-        for p in product(a, repeat=i):
+        for p in product(a, i):
             temp.append(''.join(p))
     temp.sort()
     answer = temp.index(word) + 1
