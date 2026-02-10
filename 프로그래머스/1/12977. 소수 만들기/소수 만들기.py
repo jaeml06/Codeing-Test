705574d60b530a9ece496af4193b72aedb1e259e
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-10 14:07:30 +0900

    [level 1] Title: 소수 만들기, Time: 34.96 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -1,16 +1,30 @@
-from itertools import combinations
 def is_prime(n):
     if n < 2:
         return False
-    limit = int(n ** 0.5)
-    for i in range(2, limit + 1):
+    end = int(n ** 0.5)
+    for i in range(2, end + 1):
         if n % i == 0:
             return False
     return True
+
+def combination(arr, cnt):
+    result = []
+    path = []
+    def dfs(start):
+        if len(path) == cnt:
+            result.append(path[:])
+            return
+        for i in range(start, len(arr)):
+            path.append(arr[i])
+            dfs(i + 1)
+            path.pop()
+    dfs(0)
+    return result
+        
         
 def solution(nums):
     answer = 0
-    for val in combinations(nums, 3):
+    for val in combination(nums, 3):
         s = sum(val)
         if is_prime(s):
             answer += 1
