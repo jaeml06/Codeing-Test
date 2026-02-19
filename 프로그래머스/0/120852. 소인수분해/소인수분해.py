6cf83e12a3f29a06d225c1b199745b1d4e88b0b9
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-19 17:14:43 +0900

    [level 0] Title: 소인수분해, Time: 0.78 ms, Memory: 9.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = []
+    div = 2
+    while n > 1:
+        if n % div == 0:
+            answer.append(div)
+            n//=div
+        else:
+            div+= 1
+            
+    return sorted(list(set(answer)))
