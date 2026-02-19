c14f5379bb794dd2cdd148c0b620424b68f39dd0
JAEMIN LEE <jaeml0630@gmail.com>
2026-02-19 20:05:46 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,8 +1,3 @@
 def solution(my_string):
-    seen = set()
-    temp = []
-    for c in my_string:
-        if c not in seen:
-            seen.add(c)
-            temp.append(c)
-    return ''.join(temp)
+    answer = ''
+    return answer
