f08e80707cdeb51a489461af829e2db80b9fffee
JAEMIN LEE <jaeml0630@gmail.com>
2026-03-05 16:14:04 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -0,0 +1,10 @@
+n = int(input())
+dance = {"ChongChong"}
+
+for _ in range(n):
+    a, b = input().split()
+    if a in dance or b in dance:
+        dance.add(a)
+        dance.add(b)
+
+print(len(dance))
