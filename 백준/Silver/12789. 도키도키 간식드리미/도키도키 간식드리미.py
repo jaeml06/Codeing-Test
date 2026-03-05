8ffacd986890d955e725b30cfe45f2874c6368ea
JAEMIN LEE <jaeml0630@gmail.com>
2026-03-05 13:55:18 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+stack = []
+need = 1
+
+for x in arr:
+    while stack and stack[-1] == need:
+        stack.pop()
+        need+=1
+    if x == need:
+        need+=1
+    else:
+        stack.append(x)
+while stack and stack[-1] == need:
+    stack.pop()
+    need += 1
+
+print('Nice' if need == n +1 else 'Sad')
