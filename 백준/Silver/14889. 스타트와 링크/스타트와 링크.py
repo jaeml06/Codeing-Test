672df1f52659c652f9ccc417993d3c3962fc42af
JAEMIN LEE <jaeml0630@gmail.com>
2026-03-16 18:48:04 +0900

    [Silver I] Title: 스타트와 링크, Time: 3264 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,25 +1,22 @@
 import sys
 n = int(input())
-
 grid = [list(map(int, input().split())) for _ in range(n)]
 visited = [False] * n
 answer = sys.maxsize
+
 def dfs(idx, cnt):
     global answer
     if cnt == n // 2:
-        start = 0
+        star = 0
         link = 0
-        
         for i in range(n):
             for j in range(i + 1, n):
                 if visited[i] and visited[j]:
-                    start += grid[i][j] + grid[j][i]
+                    star += grid[i][j] + grid[j][i]
                 elif not visited[i] and not visited[j]:
                     link += grid[i][j] + grid[j][i]
-        answer = min(answer, abs(start - link))
+        answer = min(answer, abs(star - link))
         return
-        
-        
     for i in range(idx, n):
         if not visited[i]:
             visited[i] = True
