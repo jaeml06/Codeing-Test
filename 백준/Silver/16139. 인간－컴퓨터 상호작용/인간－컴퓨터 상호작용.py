f49e1ad5f88ace2ef7dd785054eb828d28754941
JAEMIN LEE <jaeml0630@gmail.com>
2026-03-26 11:45:41 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 48 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,14 +1,18 @@
-s = input()
+import sys
+input = sys.stdin.readline
+
+s = input().strip()
 q = int(input())
 
 n = len(s)
+
 prefix = [[0] * (n + 1) for _ in range(26)]
 
 for i in range(n):
     for j in range(26):
         prefix[j][i + 1] = prefix[j][i]
     prefix[ord(s[i]) - ord('a')][i + 1] += 1
-    
+
 for _ in range(q):
     a, l, r = input().split()
     l = int(l)
