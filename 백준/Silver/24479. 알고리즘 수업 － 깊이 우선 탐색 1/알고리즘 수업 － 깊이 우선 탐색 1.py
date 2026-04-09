8c06289dd1dd375ddab522c7b7cd2de89398c152
JAEMIN LEE <jaeml0630@gmail.com>
2026-04-09 11:27:12 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 596 ms, Memory: 69288 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+n, m, r = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+for i in range(1, n + 1):
+    graph[i].sort()
+
+visited = [0] * (n + 1)
+order = 1
+def dfs(x):
+    global order
+    visited[x] = order
+    for nxt in graph[x]:
+        if visited[nxt] == 0:
+            order+=1
+            dfs(nxt)
+dfs(r)
+for i in range(1, n + 1):
+    print(visited[i])
+    
+    
+    
+
+  
