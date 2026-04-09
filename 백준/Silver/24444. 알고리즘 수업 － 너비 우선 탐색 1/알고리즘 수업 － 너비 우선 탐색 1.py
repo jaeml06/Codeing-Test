d0364b160e20446d9d6cd2925299a5db1d242d57
JAEMIN LEE <jaeml0630@gmail.com>
2026-04-09 11:58:47 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 544 ms, Memory: 60904 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+
+grid = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    grid[u].append(v)
+    grid[v].append(u)
+
+for i in range(1, n + 1):
+    grid[i].sort()
+
+visited = [0] * (n + 1)
+order = 1
+
+def bfs(x):
+    global order
+    q = deque([x])
+    visited[x] = order
+    
+    while q:
+        cur = q.popleft()
+        
+        for nxt in grid[cur]:
+            if visited[nxt] == 0:
+                order += 1
+                visited[nxt] = order
+                q.append(nxt)
+bfs(r)
+
+for i in range(1, n + 1):
+    print(visited[i])
+                
+    
