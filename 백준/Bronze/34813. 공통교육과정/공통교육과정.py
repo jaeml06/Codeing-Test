552566731df6d3fbcfe5d60c70a635eb9d876404
JAEMIN LEE <jaeml0630@gmail.com>
2026-04-10 10:24:43 +0900

    [Bronze V] Title: 공통교육과정, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,10 @@
+temp = input()
+
+if temp[0] == 'F':
+    print('Foundation')
+elif temp[0] == 'C':
+    print('Claves')
+elif temp[0] == 'V':
+    print('Veritas')
+elif temp[0] == 'E':
+    print('Exploration')
