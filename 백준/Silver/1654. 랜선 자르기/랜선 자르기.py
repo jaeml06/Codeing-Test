d689dfec659a9f08b525b3b08258c33b0813d048
JAEMIN LEE <jaeml0630@gmail.com>
2026-04-13 15:41:00 +0900

    [Silver II] Title: 랜선 자르기, Time: 104 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+k, n = map(int, input().split())
+lines = [int(input()) for _ in range(k)]
+
+left = 1
+right = max(lines)
+answer = 0
+
+while left <= right:
+    mid = (left + right) // 2
+    cnt = 0
+    for line in lines:
+        cnt += line // mid
+    if cnt >= n:
+        answer = mid
+        left = mid + 1
+    else:
+        right = mid - 1
+print(answer)
+    
