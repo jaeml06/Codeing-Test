bb12b39a0d7e33e9b65fb4f7dfde78220cd3c7bb
JAEMIN LEE <jaeml0630@gmail.com>
2026-04-19 20:10:09 +0900

    [level 2] Title: 주식가격, Time: 26.09 ms, Memory: 18.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,14 +1,15 @@
 def solution(prices):
     n = len(prices)
     answer = [0] * n
-    stack = []
+    s = []
+    
     for i in range(n):
-        while stack and prices[i] < prices[stack[-1]]:
-            j = stack.pop()
-            answer[j] = i - j
-        stack.append(i)
-        
-    while stack:
-        j = stack.pop()
-        answer[j] = n - 1 - j
+        while s and prices[i] < prices[s[-1]]:
+            temp = s.pop()
+            answer[temp] = i - temp
+        s.append(i)
+    
+    while s:
+        temp = s.pop()
+        answer[temp] = n - 1- temp
     return answer
