1bc1043fb950b0dbb28fed6722375265ba6a2f52
JAEMIN LEE <jaeml0630@gmail.com>
2026-04-22 22:31:28 +0900

    Title: 함수를 이용한 소수 판별, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260422/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+cnt = 0
+for i in range(a, b + 1):
+    if is_prime(i):
+        cnt += i
+
+print(cnt)
